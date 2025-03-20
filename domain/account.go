c7b987086def7a7e8be927d258c17f1ca16e74e1
platon <platonpechenev@ya.ru>
2025-03-20 10:19:25 +0300

    init

--- domain/account.go
+++ domain/account.go
@@ -3,5 +3,6 @@ package domain
 type BankAccount struct {
 	id      uint32
 	name    string
-	balance int32
+	balance float64
 }
+
--- domain/logic.go
+++ domain/logic.go
@@ -1,24 +1,32 @@
 package domain
 
+import "io"
+
 func PerformOperation(operation Operation) {
 	// pull account and category
 	// validate: category matches operation and account has sufficient funds
 }
 
-func CreateCategory()   {}
-func DeleteCategory()   {}
-func EditCategoryName() {}
+func CreateCategory(entity Category)          {}
+func DeleteCategory(id uint32)                {}
+func EditCategoryName(id uint32, name string) {}
 
-func CreateAccount()      {}
-func EditAccountBalance() {}
-func EditAccountName()    {}
-func DeleteAccount()      {}
+func CreateAccount(account BankAccount)                {}
+func EditAccountBalance(id uint32, newBalance float64) {}
+func EditAccountName(id uint32, newName string)        {}
+func DeleteAccount(id uint32)                          {}
 
-func CreateOperation()     {}
-func DeleteOperation()     {}
-func EditOperationAmount() {}
+func CreateOperation(operation Operation)              {}
+func DeleteOperation(id uint32)                        {}
+func EditOperationAmount(id uint32, newAmount float64) {}
 
-func RecalculateAmount() {}
+func RecalculateAmount(accountId uint32) {}
 
-func DumpJson() {}
-func LoadJson() {}
+type GlobalState struct {
+	Accounts   []BankAccount
+	Operations []Operation
+	Categories []Category
+}
+
+func Dump(state GlobalState, writer io.Writer)
+func Load(reader io.Reader) GlobalState
